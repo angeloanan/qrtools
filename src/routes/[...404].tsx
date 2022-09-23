2012ae5538cb45e66a666b1219c0e2fbe8a53d22
Christopher Angelo <angelo@angeloanan.xyz>
2022-09-24 04:32:16 +0700

    feat: init

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,25 @@
+import { Link } from '@solidjs/router'
+
+export default function NotFound() {
+  return (
+    <main class='mx-auto p-4 text-center text-gray-700'>
+      <h1 class='max-6-xs my-16 text-6xl font-thin uppercase text-sky-700'>Not Found</h1>
+      <p class='mt-8'>
+        Visit{' '}
+        <Link href='https://solidjs.com' target='_blank' class='text-sky-600 hover:underline'>
+          solidjs.com
+        </Link>{' '}
+        to learn how to build Solid apps.
+      </p>
+      <p class='my-4'>
+        <Link href='/' class='text-sky-600 hover:underline'>
+          Home
+        </Link>
+        {' - '}
+        <Link href='/about' class='text-sky-600 hover:underline'>
+          About Page
+        </Link>
+      </p>
+    </main>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,56 @@
+import { createEffect, createSignal, onMount } from 'solid-js'
+import QRCode from 'qrcode'
+import { Link } from 'solid-start'
+
+const defaultLink = 'https://qr.angelo.fyi'
+
+const updateQr = (canvas: HTMLCanvasElement, text: string) => {
+  QRCode.toCanvas(canvas, text, (error) => {
+    if (error) console.error(error)
+  })
+}
+
+export default function Home() {
+  let qrCanvas
+  const [text, setText] = createSignal(defaultLink)
+
+  onMount(() => {
+    updateQr(qrCanvas, text())
+  })
+
+  createEffect(() => {
+    updateQr(qrCanvas, text())
+  })
+
+  return (
+    <>
+      <main class='mx-auto p-4 text-center text-gray-700'>
+        <h1 class='max-6-xs my-16 text-6xl font-thin uppercase text-sky-700'>QR Code Tool</h1>
+
+        {/* Main content below */}
+
+        <div class='flex flex-col items-center'>
+          <textarea
+            autofocus
+            class='rounded p-2 outline outline-1 outline-gray-300'
+            placeholder='Type something here!'
+            oninput={(e) => {
+              console.log(e.currentTarget.value)
+              setText(e.currentTarget.value == '' ? defaultLink : e.currentTarget.value)
+            }}
+          />
+          <canvas ref={qrCanvas} class='my-2'></canvas>
+        </div>
+
+        {/* Footer */}
+      </main>
+      <footer class='mx-auto p-4 text-center text-gray-700'>
+        Created by{' '}
+        <a href='https://angeloanan.xyz' class='text-sky-700 underline'>
+          Angelo
+        </a>{' '}
+        at 4 AM 💀
+      </footer>
+    </>
+  )
+}
